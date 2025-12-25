52ffe300ed72f178c004b705f707d245263c733c
Nicolás Sabogal <ndsabogalv@gmail.com>
2025-12-25 15:06:44 -0500

    feat: create reusable s3 and dynamodb modules

---
+++ terraform/modules/s3/main.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "this" {
+  bucket        = var.bucket_name
+  force_destroy = var.force_destroy
+  tags          = var.tags
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = var.versioning_enabled ? "Enabled" : "Suspended"
+  }
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+  acl    = var.acl
+}
